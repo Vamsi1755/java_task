a3950cda6f9638d82ca3e8f1e8f9230cc93a69b7
Vamsi1755 <karanivamsi05@gmail.com>
2026-07-06 12:51:19 +0530

    Add files via upload

---
+++ task_2.java
@@ -0,0 +1,19 @@
+package com.code;
+
+public class task_2 {
+
+	public static void main(String[] args) {
+		
+		int a = 7;
+		
+		int b = 8;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+		
+	}
+
+}
