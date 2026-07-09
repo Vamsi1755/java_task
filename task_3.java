60c723ae55786892e0d301bc23a9acf02ed292fc
Vamsi1755 <karanivamsi05@gmail.com>
2026-07-09 16:18:45 +0530

    Add files via upload

---
+++ task_3.java
@@ -0,0 +1,17 @@
+package com.code;
+
+public class task_3 {
+
+	public static void main(String[] args) {
+		int m1 = 80;
+		int m2 = 90;
+		
+		System.out.println("marks1<marks2 :" +( m1<m2));
+		System.out.println("marks1=marks2 : "+ (m1=m2));
+		System.out.println("marks1<=marks2 :"+(m1<=m2));
+		System.out.println("marks1==marls2 :"+ (m1==m2));
+		System.out.println("marks1!=marks2 :"+ (m1!=m2));
+
+	}
+
+}
